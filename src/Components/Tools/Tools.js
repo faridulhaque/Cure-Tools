86caade6c72c47191e1b436c13366e3622f02c94
faridulhaque <faridulhaquemurshed@gmail.com>
2022-05-23 01:34:11 +0600

    tools done

--- src/Components/Tools/Tools.js
+++ src/Components/Tools/Tools.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import './Tools.css';
+import React, { useEffect, useState } from "react";
+import "./Tools.css";
 
 const Tools = () => {
-    return (
-        <div>
-            <h2 className="text-center mt-5">Equipments</h2>
-            <div className='cards'>
+  const [tools, setTools] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/tools")
+      .then((res) => res.json())
+      .then((data) => setTools(data));
+  }, []);
+  return (
+    <div className="mb-5">
+      <h2 className="text-center mt-5 mb-5">Equipments</h2>
+      <div className="cards">
+          {
+              tools.slice(0, 3).map((tool) =>(
+                  <div className="single-card">
+                      <img className="img-tool" src={tool.img} alt=""/>
+                      <div className="btn-tool-wrapper">
 
-            </div>
+                      <button className="btn-tool"><i className="fa-solid fa-plus"></i></button>
+                      </div>
+                      <p className='tool-name'>{tool.name}</p>
+                      <div className="quantities">
+                          <div className='quantities-tool-wrapper'>
+                              <p className="quantities-tool-item">Price</p>
+                              <p className="quantities-tool-value">$ {tool.price}</p>
+                          </div>
+                          <div className='quantities-tool-wrapper'>
+                              <p className="quantities-tool-item">Available Quantity</p>
+                              <p className="quantities-tool-value">{tool.avlQuantity} <sub>unit</sub> </p>
+                          </div>
+                          <div className="quantities-tool-wrapper">
+                              <p className="quantities-tool-item">Minimum Order Quantity</p>
+                              <p className="quantities-tool-value">{tool.minQuantity} <sub>unit</sub></p>
+                          </div>
+                      </div>
+                      
+                  </div>
+              ))
+          }
         </div>
-    );
+    </div>
+  );
 };
 
 export default Tools;
