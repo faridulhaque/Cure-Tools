345050c174330c88eae74b33d1010d574e34ff0b
faridulhaque <faridulhaquemurshed@gmail.com>
2022-06-07 16:10:21 +0600

    responsive for user interface done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
+        <div className="ms-3">
             <div>
                 <h2>How will you improve the performance of a React Application?</h2>
                 <p>
